4ebffb97e0b7fca4fd55283301f83670d56978e4
wisie <wisnia501@gmail.com>
2023-07-08 16:19:12 +0200

    fix bug - fail response was send despite a success

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -68,15 +68,12 @@ exports.updateTour = catchAsync(async (req, res, next) => {
 
 exports.deleteTour = catchAsync(async (req, res, next) => {
   const tour = await Tour.findByIdAndDelete(req.params.id);
+  if (!tour) {
+    return next(new AppError(`No tour found with ID ${req.params.id}`, 404));
+  }
   res.status(204).json({
     status: 'success',
     data: null,
-  });
-  if (!tour)
-    return next(new AppError(`No tour found with ID ${req.params.id}`, 404));
-  res.status(404).json({
-    status: 'fail',
-    message: 'Tour not found',
   });
 });
 
