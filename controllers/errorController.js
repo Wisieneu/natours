4ca0e0bdfe38110b12681338a6eafcfcbad5fb94
wisie <wisnia501@gmail.com>
2023-07-29 02:37:11 +0200

    implement general frontend error rendering

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -23,30 +23,55 @@ const handleJWTError = () =>
 const handleJWTExpiredError = () =>
   new AppError('Your login token has expired. Please log in again.', 401);
 
-const sendErrorDev = (err, res) => {
-  res.status(err.statusCode).json({
-    status: err.status,
-    error: err,
-    message: err.message,
-    stack: err.stack,
-  });
-};
-
-const sendErrorProd = (err, res) => {
-  if (err.isOperational) {
-    // Operational, trusted error: send message to client
+const sendErrorDev = (err, req, res) => {
+  // API error handling
+  if (req.originalUrl.startsWith('/api')) {
     res.status(err.statusCode).json({
       status: err.status,
+      error: err,
       message: err.message,
+      stack: err.stack,
     });
   } else {
+    // Rendered website error handling
+    res.status(err.statusCode).render('error', {
+      title: 'Error',
+      msg: err.message,
+    });
+  }
+};
+
+const sendErrorProd = (err, req, res) => {
+  // A) API error handling
+  if (req.originalUrl.startsWith('/api')) {
+    if (err.isOperational) {
+      // Operational, trusted error: send message to client
+      return res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message,
+      });
+    }
     // Programming or other unknown error: don't leak error details
     console.error('Error 💥💥💥:', err);
-    res.status(500).json({
+    return res.status(500).json({
       status: 'error',
       message: 'An unknown error has occurred. Please try again later.',
     });
   }
+  // B) Rendered website error handling
+  if (err.isOperational) {
+    // Operational, trusted error: send message to client
+    return res.status(err.statusCode).render('error', {
+      title: 'Error',
+      msg: err.message,
+    });
+  }
+  // Programming or other unknown error: don't leak error details
+  console.error('Error 💥💥💥:', err);
+  res.status(500).json({
+    title: 'Error',
+    message: 'An unknown error has occurred. Please try again later.',
+  });
 };
 
 module.exports = (err, req, res, next) => {
@@ -54,14 +79,15 @@ module.exports = (err, req, res, next) => {
   err.status = err.status || 'error';
 
   if (process.env.NODE_ENV === 'development') {
-    sendErrorDev(err, res);
+    sendErrorDev(err, req, res);
   } else if (process.env.NODE_ENV === 'production') {
     let error = { ...err };
+    error.message = err.message;
     if (err.name === 'CastError') error = handleCastErrorDB(err);
     if (err.name === 'ValidationError') error = handleValidationErrorDB(err);
     if (err.code === 11000) error = handleDuplicateFieldsDB(err);
     if (err.name === 'JsonWebTokenError') error = handleJWTError();
     if (err.name === 'TokenExpiredError') error = handleJWTExpiredError();
-    sendErrorProd(error, res);
+    sendErrorProd(error, req, res);
   }
 };
