c06cb4bbc0b3140728c0b9802b3682587241bdad
wisie <b.wisn7@gmail.com>
2024-07-21 16:17:18 +0000

    downgrade sharp for prod linux usage

--- server.js
+++ server.js
@@ -15,10 +15,7 @@ const DB = process.env.DATABASE.replace(
   process.env.DATABASE_PASSWORD,
 );
 mongoose
-  .connect(DB, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(DB)
   .then(() => console.log('DB connection successful!'));
 
 const port = process.env.PORT || 8000;
